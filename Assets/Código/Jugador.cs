b014db07667e0d3634eafd0f18229dab984b530a
1d4niGIT <ronaldds15@gmail.com>
2026-06-15 19:59:00 -0500

    Rehaciendo

--- Assets/Código/Jugador.cs
+++ Assets/Código/Jugador.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 public class Jugador : MonoBehaviour
 {
-    public float VelocidadJugador = 5f;
+    public float VelocidadJugador = 2f;
     public float Vida = 100f;
 
     void Update()
